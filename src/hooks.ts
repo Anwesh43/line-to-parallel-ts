6fffc0036186717a746e085bdcd7e9eea1deb1f1
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 21:17:00 +0530

    adding util methods

--- src/hooks.ts
+++ src/hooks.ts
@@ -39,7 +39,7 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
@@ -48,3 +48,7 @@ export const useDimension = () => {
         h,
     }
 }
+
+const maxScale = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
