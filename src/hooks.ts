f2b412b9750580918874bc7e7135cad141b9ff70
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 21:27:18 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,5 @@
 import {
+    CSSProperties,
     useEffect, 
     useState 
 } from 'react'
@@ -52,3 +53,40 @@ export const useDimension = () => {
 const maxScale = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
 const divideScale = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
 const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+const useStyle = (w : number, h : number, scale : number) => {
+    const sf : number = sinify(scale)
+    const sf1 : number = divideScale(sf, 0, 2)
+    const sf2 : number = divideScale(sf, 1, 2)
+    const position = 'absolute'
+    const background = 'indigo'
+    const size : number = Math.min(w, h)  / 5
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            const transform = `rotate(${90 * sf2}deg)`
+            return {
+                left, 
+                top, 
+                position, 
+                transform 
+
+            }
+        }, 
+        lineStyle(i : number) : CSSProperties {
+            const top = `${-size * 0.5 * (1 - 2 * i)}px`
+            const left = `${-size / 2}px`
+            const width = `${size}px`
+            const height = `${Math.min(w, h) / 80}px`
+            return {    
+                top, 
+                left, 
+                width, 
+                height, 
+                background, 
+                position 
+            }
+        }
+    }
+}
